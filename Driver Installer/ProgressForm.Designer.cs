266c172a9e6892bc44bb8a042bb0b2db9c3f035a
TheZivo <TheZivo@DESKTOP-I3E4PO6>
2019-04-16 07:08:48 -0400

    scanner complete other than progress bar, working on spool starter

---
+++ Driver Installer/ProgressForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Driver_Installer
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadingBar = new System.Windows.Forms.ProgressBar();
+            this.cancelOp = new System.Windows.Forms.Button();
+            this.ProcessingText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LoadingBar
+            // 
+            this.LoadingBar.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.LoadingBar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LoadingBar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.LoadingBar.Location = new System.Drawing.Point(100, 176);
+            this.LoadingBar.MarqueeAnimationSpeed = 50;
+            this.LoadingBar.Maximum = 255;
+            this.LoadingBar.Name = "LoadingBar";
+            this.LoadingBar.Size = new System.Drawing.Size(590, 47);
+            this.LoadingBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.LoadingBar.TabIndex = 0;
+            // 
+            // cancelOp
+            // 
+            this.cancelOp.Location = new System.Drawing.Point(577, 346);
+            this.cancelOp.Name = "cancelOp";
+            this.cancelOp.Size = new System.Drawing.Size(75, 23);
+            this.cancelOp.TabIndex = 1;
+            this.cancelOp.Text = "Cancel";
+            this.cancelOp.UseVisualStyleBackColor = true;
+            // 
+            // ProcessingText
+            // 
+            this.ProcessingText.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold);
+            this.ProcessingText.Location = new System.Drawing.Point(289, 66);
+            this.ProcessingText.Name = "ProcessingText";
+            this.ProcessingText.Size = new System.Drawing.Size(215, 31);
+            this.ProcessingText.TabIndex = 2;
+            this.ProcessingText.Text = "Processing Please Wait";
+            this.ProcessingText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ProcessingText);
+            this.Controls.Add(this.cancelOp);
+            this.Controls.Add(this.LoadingBar);
+            this.Name = "ProgressForm";
+            this.Text = "ProgressForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar LoadingBar;
+        private System.Windows.Forms.Button cancelOp;
+        private System.Windows.Forms.TextBox ProcessingText;
+    }
+}
--- Driver Installer/TechToolsForm.cs
+++ Driver Installer/TechToolsForm.cs
@@ -19,6 +19,7 @@ namespace Driver_Installer
 {
     public partial class TechToolsForm : Form
     {
+        //declare thread for scanner and context for mainthread
         public Thread scanTh;
         public SynchronizationContext MainThread;
 
@@ -57,158 +58,149 @@ namespace Driver_Installer
 
         }
 
+        public static ConcurrentDictionary<string, string> found = new ConcurrentDictionary<string, string> { };
+        public static ConcurrentBag<string> notFound = new ConcurrentBag<string>();
+        public static ConcurrentBag<string> occupied = new ConcurrentBag<string>();
+
         /* Used to ping the current network for occupied IP addresses and get their Hostname back if possible
          Some equipment will not provide a hostname back, typically networking equipment and VoIP phones. 
          As the network scanner is being used to show available IPs and Machines these values will be added to a seperate
          dictionary to be printed after. 
         */
         public void Scanner()
-        {
+        {            
+            //init this thread as main
             MainThread = SynchronizationContext.Current;
             if (MainThread == null) MainThread = new SynchronizationContext();
+
+            // create new instance of loading screen
+
+            ProgressForm running = new ProgressForm();
+            running.Show();
+            
+            //Runs scanLoop to fill dictionaries and bags
+            scanLoop();        
+            
+            //When scanLoop ends the loading screen is closed
+            running.Close();
+
+            //create new instance of resultform for host/ip pairs to display
             ResultForm pingsweep = new ResultForm();
             pingsweep.Show();
+
+            pingsweep.resbox.Text = "Found the following hosts:" + Environment.NewLine + Environment.NewLine;
+            foreach (var iterHost in found.OrderBy(x => x.Key).Select(x => x.Value))
+            {
+                pingsweep.resbox.AppendText(iterHost + Environment.NewLine);
+            }
+
+            pingsweep.resbox.AppendText
+                (Environment.NewLine + "Found the following Occupied IP's:" + Environment.NewLine + Environment.NewLine);
+            foreach (var takenIP in occupied.OrderBy(x => x))
+            {
+                pingsweep.resbox.AppendText(takenIP + Environment.NewLine); 
+            };
+
+            found = new ConcurrentDictionary<string, string>();
+            occupied = new ConcurrentBag<string>();
+
+
+        }
+
+         public void scanLoop()
+            {
+            //declare variables used in loop
             Ping pingTest;
             PingReply reply;
             IPAddress addy;
             IPHostEntry host;
-
             var subnet = getsubnet();
-            var found = new ConcurrentDictionary<string, string>();
-            var notFound = new ConcurrentBag<string>();
-            var occupied = new ConcurrentBag<string>();
-
+            
+            //loop through IP's on the current subnet ping each then store result in correct dict or bag
             Parallel.For(0, 256, (i) =>
-            {
-                string itSubnet = "." + i.ToString();
-                pingTest = new Ping();
-                reply = pingTest.Send(subnet + itSubnet, 300);
-                if (reply.Status == IPStatus.Success)
                 {
-                    try
+                    //Pinging
+                    string itSubnet = "." + i.ToString();
+                    pingTest = new Ping();
+                    reply = pingTest.Send(subnet + itSubnet, 300);
+
+                    //Storing
+                    if (reply.Status == IPStatus.Success)
                     {
-                        addy = IPAddress.Parse(subnet + itSubnet);
-                        host = Dns.GetHostEntry(addy);
-                        found.TryAdd(subnet + itSubnet, host.HostName.ToString());
+                        try
+                        {
+                            addy = IPAddress.Parse(subnet + itSubnet);
+                            host = Dns.GetHostEntry(addy);
+                            found.TryAdd(subnet + itSubnet, host.HostName.ToString());
+                        }
+                        catch (Exception)
+                        {
+                            occupied.Add(subnet + itSubnet);
+                        }
                     }
-                    catch (Exception)
+
+                    else
                     {
-                       occupied.Add(subnet + itSubnet);
+                        notFound.Add(subnet + itSubnet);
                     }
-                }
-                else
-                {
-                    
-                    notFound.Add(subnet + itSubnet);
-                }
-            });
+                });
+            }                    
 
-            MainThread.Send((object state) => {
-                pingsweep.resbox.AppendText("Found the following hosts: " + System.Environment.NewLine);
-
-
-/*
-                foreach (KeyValuePair<string, string> kvp in found.OrderBy(x => x.Key))
-                {
-                    pingsweep.resbox.AppendText(kvp.Value + " at " + kvp.Key + System.Environment.NewLine);
-                }
-*/                               
-            }, null); 
-                                                                        
-/*
-            for ( int i = 0; i < 254; i++)
-            {
-                string itSubnet = "." + i.ToString();
-                pingTest = new Ping();
-                reply = pingTest.Send(subnet + itSubnet, 300);
-                var s = "";
-                if (reply.Status == IPStatus.Success)
-                {
-                    try
-                    {
-                        addy = IPAddress.Parse(subnet + itSubnet);
-                        host = Dns.GetHostEntry(addy);
-                        s = subnet + itSubnet + host.HostName.ToString() + System.Environment.NewLine;
-                        pingsweep.resbox.AppendText(s);
-                        //found.TryAdd(itSubnet, addy.ToString());
-                    }
-                    catch (Exception e)
-                    {
-                        pingsweep.resbox.AppendText(e.Message + ": " + subnet + itSubnet + System.Environment.NewLine);
-                    }
-                }
-                else
-                {
-                    pingsweep.resbox.AppendText(subnet + itSubnet + " Inactive" + System.Environment.NewLine);
-                    //notFound.TryAdd(itSubnet, subnet + itSubnet);
-                }
-            };
-
-
-            //Console.WriteLine("Found the following hosts: ");
-            //foreach (var iterHost in found.OrderBy(x => x.Key).Select(x => x.Value))
-            //{
-              //  Console.WriteLine(iterHost);
-            //}
-            //Console.ReadLine();
-*/
-        }
-    
-
-        private void TechToolsForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        private void TechToolsForm_Load(object sender, EventArgs e) { }
+        
+        private void label4_Click(object sender, EventArgs e) { }
+        private void PingIP_TextChanged(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
 
         //Runs an IP config /all command to provide network information
 
         private void ipconfigBtn_Click(object sender, EventArgs e)
         {
             Cmdline("ipconfig", " /all");
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
+        }    
 
         //Runs a ping test to provided IP, after parsing the text and checking for correct input.
         private void pingBtn_Click(object sender, EventArgs e)
         {
-
             //CHECK INPUT 
             string ip = PingIP.Text;
             Cmdline("ping", ip);
         }
 
-        private void PingIP_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         // Runs network scan of current network by passing getsubnet function to Scanner 
         public void netScanBtn_Click(object sender, EventArgs e)
         {
-            scanTh = new Thread(new ThreadStart(Scanner));
-            scanTh.Start();
-
-
+            Scanner();                       
         }
+
         // Stops then restarts the Print Spooler, something people constantly tell me helps but I have never actually needed..
         private void spoolBtn_Click(object sender, EventArgs e)
         {
-            // Make work 
-            Cmdline("net stop spooler", "");
+            
+
+            Process cmd = new Process();
+            ResultForm spoolrun = new ResultForm();
+            spoolrun.Show();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = false;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine("cd c:");
+            cmd.StandardInput.WriteLine("net start spooler");
+            //cmd.StandardInput.WriteLine("net start spooler");
+            //cmd.StandardInput.WriteLine("exit");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            spoolrun.resbox.Text = cmd.StandardOutput.ReadToEnd();
+
+
+
+
+            //Cmdline("net stop spooler", "");
         }
     }    
 }
